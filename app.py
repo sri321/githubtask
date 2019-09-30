6783a58a261144ea157a077d19354f6cfac7f722
Sri <stalin231@gmail.com>
2019-09-30 18:38:35 -0400

    updated to fix erros

--- app.py
+++ app.py
@@ -2,16 +2,21 @@ import requests
 import json
 from flask import Flask
 
-#### Get the Name from API ####
-data = requests.get('http://uinames.com/api/').json()
+def getdata():
+    try:
+        #### Get the Name from API ####
+        data = requests.get('http://uinames.com/api/').json()
+        #### get Joke from API ####
+        jokedat = requests.get('http://api.icndb.com/jokes/random?firstName=John&lastName=Doe&limitTo=[nerdy]').json()
+        #### Convert data into required format ####
+        output = (jokedat['value']['joke'].replace('John Doe',data['name']+' '+data['surname'])).encode('ascii', 'ignore')
+    except:
+        data = requests.get('http://uinames.com/api/')
+        if data.status_code == 429:
+            output = "Too Many Requests please wait and Try again"
+    return output
 
-#### get Joke from API ####
-jokedat = requests.get('http://api.icndb.com/jokes/random?firstName=John&lastName=Doe&limitTo=[nerdy]').json()
-
-#### Convert data into required format ####
-output = (data['name']+' '+data['surname']+' '+jokedat['value']['joke']).encode('ascii', 'ignore')
-
-#### Testing API output####
+#### Testing API output ####
 #print (output)
 
 #### Start WebService ####
@@ -20,7 +25,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     html = "<h3>{name}!</h3>"
-    return html.format(name=output)
+    return html.format(name=getdata())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
