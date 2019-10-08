8c59a4c2791cf2ae6a2a71fb427b683575621e8c
Sri <stalin231@gmail.com>
2019-10-08 11:29:41 -0400

    test

---
+++ spark.py
@@ -0,0 +1,5 @@
+import pyspark
+import pandas as pd
+sdata = pd.read_csv("C:/Users/sripa/Downloads/download/Granify_Data_Eng_Task/sessions.csv")
+fdata = pd.read_csv("C:/Users/sripa/Downloads/download/Granify_Data_Eng_Task/features.csv")
+odata = pd.read_csv("C:/Users/sripa/Downloads/download/Granify_Data_Eng_Task/orders.csv")
